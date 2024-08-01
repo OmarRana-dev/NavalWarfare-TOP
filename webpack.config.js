90cb570727439a7503d543ed385995a78d26b6d5
Omar Rana <110420500+OmarRana-dev@users.noreply.github.com>
2024-08-01 22:40:30 +0500

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    static: "./dist",
+    open: true,
+    port: 5000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ca]ss$/i,
+        use: ["sass-loader"],
+      },
+    ],
+  },
+};
