db34bc7808460be8665742be0f2b5308a1ba3517
Omar Rana <110420500+OmarRana-dev@users.noreply.github.com>
2024-08-02 11:57:41 +0500

    project setup is done everything is good

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
+const name = 'James';
 
-console.log("hi");
+const person = { first: name };
+
+console.log(person);
+
+const sayHelloLinting = (fName) => {
+  console.log(`Hello linting, ${fName}`);
+};
+
+sayHelloLinting(name);
+
+const a = 3;
+const b = 5;
+console.log(a + b);
