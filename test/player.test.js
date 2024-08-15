50ad80da2c2d2d7f3f666386a896ede00c9a1714
Omar Rana <110420500+OmarRana-dev@users.noreply.github.com>
2024-08-15 23:19:13 +0500

    player file is ready after test

--- test/player.test.js
+++ test/player.test.js
@@ -1,17 +1,47 @@
 import { Player } from '../src/player';
+import { Ship } from '../src/ship';
 
-describe('Player', () => {
-  let realPlayer;
-  let computerPlayer;
+describe('Player Class', () => {
+  // Test sendAttack for a real player
+  describe('sendAttack for real player', () => {
+    let human;
+    let computer;
+    let ship;
 
-  beforeEach(() => {
-    realPlayer = new Player('real');
-    computerPlayer = new Player('computer');
+    beforeEach(() => {
+      human = new Player('real');
+      computer = new Player('computer');
+      ship = new Ship(3);
+      computer.gameboard.placeShip(ship, { x: 0, y: 0 }, 'horizontal');
+    });
+
+    test('it should receive a hit', () => {
+      const hit = human.sendAttack(computer, { x: 0, y: 0 });
+      expect(hit).toBe(true);
+    });
+
+    test('it should receive a miss', () => {
+      const miss = human.sendAttack(computer, { x: 4, y: 2 });
+      expect(miss).toBe(false);
+    });
   });
 
-  // describe("Player constructor", () => {
-  //     test("attact on opponent ship", () => {
+  // Test sendAttack for a computer player
+  describe('sendAttack for a computer player', () => {
+    let human;
+    let computer;
+    let ship;
 
-  //     })
-  // }
+    beforeEach(() => {
+      human = new Player('real');
+      computer = new Player('computer');
+      ship = new Ship(3);
+      human.gameboard.placeShip(ship, { x: 0, y: 0 }, 'vertical');
+    });
+
+    test('it should receive a hit or miss', () => {
+      const result = computer.sendAttack(human);
+      expect(typeof result).toBe('boolean');
+    });
+  });
 });
