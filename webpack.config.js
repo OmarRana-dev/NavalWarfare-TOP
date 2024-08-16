afbf56d859d2fc3b3926eff60856c5b135b523c6
Omar Rana <110420500+OmarRana-dev@users.noreply.github.com>
2024-08-17 01:28:35 +0500

    change port 5000 to 3000

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
+  mode: 'production',
+  entry: './src/index.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   devServer: {
-    static: "./dist",
+    static: './dist',
     open: true,
     port: 5000,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.s[ca]ss$/i,
-        use: ["sass-loader"],
+        use: ['sass-loader'],
       },
     ],
   },
